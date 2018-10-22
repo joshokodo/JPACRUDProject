5cd45970359383a5277946a98c59cd83d52d8311
Joshua Ciccone-Okodo <joshokodo@gmail.com>
2018-10-22 07:05:45 -0600

    added entities to db and updated README

--- DB/bountydb.sql
+++ DB/bountydb.sql
@@ -51,6 +51,11 @@ USE `bountydb`;
 INSERT INTO `bounty` (`id`, `first_name`, `last_name`, `price`, `wanted_alive`, `description`, `crime`, `nickname`) VALUES (1, 'Luffy', 'Monkey', 1500000000, 'N', 'short, black hair, wears strawhat', 'J_WALKING', 'strawhat');
 INSERT INTO `bounty` (`id`, `first_name`, `last_name`, `price`, `wanted_alive`, `description`, `crime`, `nickname`) VALUES (2, 'Zoro', 'Roranoa', 500000000, 'N', 'medium height and build, green hair, scar on eye, carries three swords', 'PUBLIC_INTOXICATION', NULL);
 INSERT INTO `bounty` (`id`, `first_name`, `last_name`, `price`, `wanted_alive`, `description`, `crime`, `nickname`) VALUES (3, 'Sanji', 'Vinsmoke', 320000000, 'N', 'blonde hair, wears suits, cooks alot', 'DISORDERLY_CONDUCT', NULL);
+INSERT INTO `bounty` (`id`, `first_name`, `last_name`, `price`, `wanted_alive`, `description`, `crime`, `nickname`) VALUES (4, 'Bobby', 'Personface', 12000000, 'Y', 'plain looking, no distiguishing features', 'VANDALISM', 'boring bull');
+INSERT INTO `bounty` (`id`, `first_name`, `last_name`, `price`, `wanted_alive`, `description`, `crime`, `nickname`) VALUES (5, 'Stephanie', 'Weird', 300000000, 'Y', 'short, red hair, pretty face', 'PETTY_THEFT', 'Kitty Cat');
+INSERT INTO `bounty` (`id`, `first_name`, `last_name`, `price`, `wanted_alive`, `description`, `crime`, `nickname`) VALUES (6, 'Sarah', 'Weird', 200000000, 'N', 'tall, red hair, scarred face', 'PETTY_THEFT', 'doggy dog');
+INSERT INTO `bounty` (`id`, `first_name`, `last_name`, `price`, `wanted_alive`, `description`, `crime`, `nickname`) VALUES (7, 'Leroy', 'Jinkens', 2000000000, 'N', ' short, out of shape, balding black hair, wears glasses', 'RECKLESS_DRIVING', 'LEEEROOOOOOY');
+INSERT INTO `bounty` (`id`, `first_name`, `last_name`, `price`, `wanted_alive`, `description`, `crime`, `nickname`) VALUES (8, 'Kevin', 'James', 100, 'Y', 'larger fellow, medium height, not that funny', 'DISORDERLY_CONDUCT', 'adam sandlers friend');
 
 COMMIT;
 
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 ## JPACRUDProject Week 8
+This project simulates a basic web based app for reading, creating, updating and
+deleting bounty entities using JPA and a Spring API configuration.
+
+##Summary
+This web app allows the user to do a general search for an entity from the home page
+(similar to a google search) or the user can do a detailed search of specific paramaters
+from detail search from the navigation above. The user also has the ability to delete or update
+an bounty found. The user can also add a new Bounty to the database from the navigation option
+on the top of each page. All bounties can be found at once at any time from the option up above
+in the navigation portion of a page.
+
+##Technologies/Techniques used
+*JPA and JPQL
+*Spring API
+*MySql
+*basic Programming practices
+*HTML, JSP
+
+
+##Lessons Learned
+The most difficult part of the project was getting a good grasp of how the JPA project
+connected to the Spring project.
